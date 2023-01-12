97f6ce19bea57c0c17437b8b6d22c6678b6e4781
Akseli Nurmio <akseli.nurmio@yle.fi>
2023-01-12 15:05:48 +0200

    Add reproduction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,21 @@
-/** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+import { GetServerSideProps } from "next"
+
+type Props = {
+  url: string;
+};
+
+export default function Home({ url }: Props) {
+  return <div>resolvedUrl: {url}</div>;
 }
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({
+  resolvedUrl
+}) => {
+  console.log(`This resolvedUrl should be URL, but is a path instead: ${resolvedUrl}`);
+
+  return {
+    props: {
+      url: new URL(resolvedUrl).toString()
+    }
+  };
+};
